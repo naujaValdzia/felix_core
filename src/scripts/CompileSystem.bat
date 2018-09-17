e41f675f01a667dc68b20f89646c4e8d6e609408
naujaValdzia <paulius.unofficial@gmail.com>
2018-09-17 15:44:35 +0300

    Changed r code location. Added errors to UI.

--- src/scripts/CompileSystem.bat
+++ src/scripts/CompileSystem.bat
@@ -1,3 +1,3 @@
 SET JSON-LOCATION=%1
 set DLC=C:\progress\openedge
-%DLC%\bin\prowin -p ../prog/analysis/systemConnect.p -basekey "INI" -ininame %2
+start /min %DLC%\bin\prowin -p ../prog/analysis/systemConnect.p -basekey "INI" -ininame %2
