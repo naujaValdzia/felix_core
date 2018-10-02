cdac6d37f045b747166632ed5b0db42a5162fe63
naujaValdzia <paulius.unofficial@gmail.com>
2018-10-02 09:10:36 +0300

    no message

--- src/scripts/startup.sh
+++ src/scripts/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 $DLC/servers/pasoe/felix/bin/tcman.sh start
-sh $DLC/servers/pasoe/felix/src/felix_core/src/scripts/loadDF.sh
+$DLC/servers/pasoe/felix/src/felix_core/src/scripts/loadDF.sh
