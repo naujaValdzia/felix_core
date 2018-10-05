35b8ccbcedad1eed1bb42bd43d39f28dc5e1ec95
naujaValdzia <paulius.unofficial@gmail.com>
2018-10-05 09:58:29 +0300

    no message

--- src/scripts/createDB.sh
+++ src/scripts/createDB.sh
@@ -2,7 +2,7 @@
 LOG=/usr/db/createDB.log
 PF=/usr/db/felixdb.pf
 DF=$DLC/servers/pasoe/felix/dev/felix_shared/db/felixdb.df
-DBLOC=/usr/db
+#DBLOC=/usr/db
 
 #create PF
 echo "-db /usr/db/felixdb.db -S 14440" > "$PF"
@@ -11,7 +11,7 @@ if [ ! -f /usr/db/felixdb.db ]
 then
 	#create empty DB
 	mkdir /usr/db
-	prodb /usr/db/usr/db/felixdb.db empty
+	prodb /usr/db/felixdb.db empty
 	proserve -pf "$PF"
 	#load DF
 	_progres -b -p "/usr/dlc/servers/pasoe/felix/dev/felix_core/src/scripts/loadDF.p" -param "$DF" -pf "$PF" > "$LOG"
